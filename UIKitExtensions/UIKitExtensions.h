141ab5a58f4f0cd122eb6472ab1c402c62089d4c
ALi <bankutia@gmail.com>
2021-12-25 18:11:14 +0100

    Initial Commit

---
+++ UIKitExtensions/UIKitExtensions.h
@@ -0,0 +1,18 @@
+//
+//  UIKitExtensions.h
+//  UIKitExtensions
+//
+//  Created by ALi on 2021. 12. 25..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitExtensions.
+FOUNDATION_EXPORT double UIKitExtensionsVersionNumber;
+
+//! Project version string for UIKitExtensions.
+FOUNDATION_EXPORT const unsigned char UIKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitExtensions/PublicHeader.h>
+
+
